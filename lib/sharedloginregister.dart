ee4109324b64ab85e39527c6874b893a72439120
Preining M <preiningmoritz@gmail.com>
2021-04-12 16:44:08 +0200

    structur changed (organizational folder added)

--- lib/sharedloginregister.dart
+++ lib/sharedloginregister.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:collection';
 import 'dart:convert';
-
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
